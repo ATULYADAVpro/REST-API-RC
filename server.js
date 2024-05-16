357e4da63789175c00d9754a2276872fbafa2643
monoj9254@gmail.com <atul088793151@gmail.com>
2024-05-16 18:11:24 +0530

    Add Student method and autoIncrement rollNo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 import { PORT } from "./src/configs/index.js";
 import studentRouter from "./src/routers/studentRouter/studentRouter.js";
-import DB_connection from './src/utils/database/connection.js'
+import DB_connection from './src/utils/database/connection.js';
 import teacharRouter from "./src/routers/teacharRouter/teacharRouter.js";
 import adminRouter from "./src/routers/adminRouter/adminRouter.js";
 // ======== Middleware ========
@@ -15,4 +15,4 @@ app.use("/admin", adminRouter);
 // ======= Connection Start =====
 DB_connection().then(() => {
     app.listen(PORT, () => { console.log(`server start http://localhost:${PORT}`) });
-}).catch((err) => { console.log("connection Fail..." + err) })
+}).catch((err) => { console.log("connection Fail..." + err) });
